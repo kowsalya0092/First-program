5ecf19452588b4a71de9967f9f7e9d3c03199c31
kowsalya0092 <kowsalya0092@gmail.com>
2020-09-16 11:35:34 +0000

    New Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
-class HelloWorld{
-	public static void main(String[] args){
-	System.out.println("Hello, World! It's me.");
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
 	}
 }
+
